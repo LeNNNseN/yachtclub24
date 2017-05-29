bbc86244fcff96899d082bf99d09866cc15e7e46
maxbpunkt <maximilian.buechsenschuetz@stud.leuphana.de>
2017-05-29 14:37:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<title>JachtClub24</title>
+        <?php 
+        include("includes/imp_assets.php"); 
+        ?>
+</head>
+
+<body id="body" data-spy="scroll" data-target=".one-page-header" class="demo-lightbox-gallery font-main promo-padding-top">
+    <main class="wrapper">
+    <?php
+    include("includes/header.html"); 
+    ?>
+        <!-- Promo Block-->
+        <section class="promo-section" id="intro">
+                <div class="container-fluid g-pr-30 g-pl-30">
+                        <!-- Owl Carousel v1-->
+                        <div class="owl2-carousel-v1 owl-theme animated slideInUp">
+                                <div class="item text-left">
+                                        <div class="course">
+                                                <img class="img-responsive" src="assets/img-temp/slider1.jpg" alt="">
+                                                <div class="course--bottom">
+                                                        <span class="course__dur">
+                                                                <i class="fa course__icon fa-calendar g-mr-10 g-mb-10"></i> Ab 10 Metern
+                                                        </span>
+                                                
+                                                        <h1 class="course__title g-mb-20 font-main"><a href="#">Segeljachten</a></h1>
+                                                        <p class="course__text g-mb-30">Luxeriöse Segeljachten zum besten Preis </p>
+                                                        <a href="segelyachten_uebersicht.php" class="btn-u course__btn-u">Zu den Segeljachten</a>
+                                                        <span class="icon-transport-93"></span>
+                                                </div>
+                                        </div>
+                                </div>
+
+                                <div class="item text-left">
+                                        <div class="course">
+                                                <img src="assets/img-temp/slider2.jpg" alt="" class="img-responsive">
+                                                <div class="course--bottom">
+                                                        <span class="course__dur">
+                                                                <i class="fa course__icon fa-calendar g-mr-10 g-mb-10"></i> Ab 6 Metern 
+                                                        </span>
+                                                        <h1 class="course__title g-mb-20 font-main"><a href="#">Yawls</a></h1>
+                                                        <p class="course__text g-mb-30">Yawls - In den schönsten Häfen</p>
+                                                        <a href="#" class="btn-u course__btn-u">Zu den Yawls</a>
+                                                </div>
+                                        </div>
+                                </div>
+
+                                <div class="item text-left">
+                                        <div class="course">
+                                                <img src="assets/img-temp/slider3.jpg" alt="" class="img-responsive">
+                                                <div class="course--bottom">
+                                                        <span class="course__dur">
+                                                                <i class="fa course__icon fa-calendar g-mr-10 g-mb-10"></i> Ab 7 Metern
+                                                        </span>
+                                                        <h1 class="course__title g-mb-20 font-main"><a href="#">Sloops</a></h1>
+                                                        <p class="course__text g-mb-30">Sloops für ruhige Gewässer</p>
+                                                        <a href="#" class="btn-u course__btn-u">Zu den Sloops</a>
+                                                </div>
+                                        </div>
+                                </div>
+
+                                <div class="item text-left">
+                                        <div class="course">
+                                                <img src="assets/img-temp/slider4.jpg" alt="" class="img-responsive">
+                                                <div class="course--bottom">
+                                                        <span class="course__dur">
+                                                                <i class="fa course__icon fa-calendar g-mr-10 g-mb-10"></i> Ab 8 Metern
+                                                        </span>
+                                                        <h1 class="course__title g-mb-20 font-main"><a href="#">Ketsch</a></h1>
+                                                        <p class="course__text g-mb-30">Eine Ketsch zum spaßigen Besegeln </p>
+                                                        <a href="#" class="btn-u course__btn-u">Zu den Ketschs</a>
+                                                </div>
+                                        </div>
+                                </div>
+
+                                <div class="item text-left">
+                                        <div class="course course--img-5">
+                                                <img src="assets/img-temp/slider5.jpg" alt="" class="img-responsive">
+                                                <div class="course--bottom">
+                                                        <span class="course__dur">
+                                                                <i class="fa course__icon fa-calendar g-mr-10 g-mb-10"></i> Ab 8 Metern
+                                                        </span>
+                                                        <h1 class="course__title g-mb-20 font-main"><a href="#">Daysailer</a></h1>
+                                                        <p class="course__text g-mb-30">Daisailer auch für den Tagesausflug</p>
+                                                        <a href="#" class="btn-u course__btn-u">Zu den Daisailern </a>
+                                                </div>
+                                        </div>
+                                </div>
+                            
+                                <div class="item text-left">
+                                        <div class="course course--img-5">
+                                                <img src="assets/img-temp/slider6.jpg" alt="" class="img-responsive">
+                                                <div class="course--bottom">
+                                                        <span class="course__dur">
+                                                                <i class="fa course__icon fa-calendar g-mr-10 g-mb-10"></i> Ab 8 Metern
+                                                        </span>
+                                                        <h1 class="course__title g-mb-20 font-main"><a href="#">Jollen</a></h1>
+                                                        <p class="course__text g-mb-30">Jollen für sonnige Tage</p>
+                                                        <a href="#" class="btn-u course__btn-u">Zu den Jollen </a>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </section>
+            <!-- End Promo Block -->
+
+
+
+
+            <br>
+            <br>
+            <br>
+            <br>
+
+
+
+            <?php
+            include("includes/footer.html");
+            ?>
+    </main>
+
+	<?php
+        include("includes/script.html");
+        ?>
+</body>
+</html>
